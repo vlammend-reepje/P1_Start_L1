c1667ff6ef7cb58ba7590cfadf88708f03c85ca7
vlammend-reepje <kennydupontx@gmail.com>
2024-09-21 12:18:23 +0200

    oefening papagaai

--- startcode/a_papegaai.py
+++ startcode/a_papegaai.py
@@ -1,2 +1,12 @@
 # Schrijf een Python-programma dat de gebruiker vraagt om een tekst in te voeren en deze tekst vervolgens afdrukt.
-# Kies een gepaste naam voor je variabele.
+# Kies een gepaste naam voor je variabelen
+print ("kevin: what yo name")
+naam = input ("you: ")
+print ("kevin: well fuck you " + naam)
+print ("you: man fuck you")
+print ("kevin: so what yo age")
+leeftijd = int(input("you: "))
+print ("kevin: so what? you're", leeftijd + int(1), "next year")
+
+
+
